f7f4ee6bde124b9a2ab87d51955d42dc572aea10
Whit128 <Whit128@users.noreply.github.com>
2017-06-29 22:49:38 -0500

    Side menu - Mobile
    Mobile side menu - javascript
    Changed background image
    Started 404 page
    Fix some errors
    
    Still need to clean code up

---
+++ test1/mobilemenu.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("navbar__side").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("navbar__side").style.width = "0";
+}
