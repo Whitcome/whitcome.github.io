e25d8fe535303074bff1b1fb3981c731b4b7c0da
Whit128 <Whit128@users.noreply.github.com>
2017-06-30 18:16:59 -0500

    New layout
    Business layout testing

--- test1/mobilemenu.js
+++ test1/mobilemenu.js
@@ -1,7 +1,9 @@
 function openNav() {
     document.getElementById("navbar__side").style.width = "100%";
+    document.getElementsByClassName("sidenav")[0].style.overflowY = "visible";
 }
 
 function closeNav() {
     document.getElementById("navbar__side").style.width = "0";
+    document.getElementsByClassName("sidenav")[0].style.overflowY = "hidden";
 }
