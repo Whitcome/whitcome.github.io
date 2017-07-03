baa61c1f820a1aa4c2a65601ace0dccf13d40769
Whit128 <Whit128@users.noreply.github.com>
2017-07-03 18:28:16 -0500

    Removed + Final
    Final Design site
    Removed old tests

--- test1/mobilemenu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("navbar__side").style.width = "100%";
-    document.getElementsByClassName("sidenav")[0].style.overflowY = "visible";
-}
-
-function closeNav() {
-    document.getElementById("navbar__side").style.width = "0";
-    document.getElementsByClassName("sidenav")[0].style.overflowY = "hidden";
-}
