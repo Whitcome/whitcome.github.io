549432c9d4db8b44227a0ce2f89d5d3af19f76dd
Whit128 <Whit128@users.noreply.github.com>
2017-10-06 14:40:16 -0500

    Parallax Scrolling for Header

---
+++ scroll.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const moving__background = $("#header__background");
+
+    window.addEventListener('scroll', function() {
+        ParallaxAnimation(moving__background);
+    });
+});
+
+function ParallaxAnimation(el) {
+    el.css('margin-top', ($(window).scrollTop())/3); // Parallax scrolling
+};
