0b1ca5658257cc634ae09b43f5e19d94e899b5a9
Dave <contact@davidwhitcome.com>
2018-08-02 18:30:44 -0500

    Comments and small changes for js

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,14 +1,17 @@
+// Preloader
 $(document).ready(function() {
     document.getElementById("loading__content").style.animation = "loading 0.8s cubic-bezier(0.6, 0.4, 0.8, 1) infinite";
     document.getElementById("body__content").style.opacity = "0";
 });
 
+// When page is loaded
 onload=function() {   
     document.getElementById("body__content").style.transition = "ease-out 0.2s opacity";
     document.getElementById("body__content").style.opacity = "1";
     setTimeout(stopLoadingAnimation, 400);
-}
+};
 
-stopLoadingAnimation=function() {
+// Stop preloader animation
+function stopLoadingAnimation() {
     document.getElementById("loading__content").style.animation = "none";
-}
+};
