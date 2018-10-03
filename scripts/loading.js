cc4fb8d3569d6d8e24c594147741cee542f4870a
Dave <contact@davidwhitcome.com>
2018-10-02 22:17:04 -0500

    Fix for some browsers executing $(document).ready after window.onload

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,11 +1,15 @@
+var pageLoaded = false;
+
 // Preloader
 $(document).ready(function() {
-    document.getElementById("loading__content").style.animation = "loading 0.8s cubic-bezier(0.6, 0.4, 0.8, 1) infinite";
-    document.getElementById("body__content").style.opacity = "0";
+    if (!pageLoaded) {
+        document.getElementById("body__content").style.opacity = "0";
+    }
 });
 
 // When page is loaded
-onload=function() {   
+window.onload = function() {
+    pageLoaded = true;
     document.getElementById("body__content").style.transition = "ease-out 0.2s opacity";
     document.getElementById("body__content").style.opacity = "1";
     setTimeout(stopLoadingAnimation, 400);
@@ -13,5 +17,5 @@ onload=function() {
 
 // Stop preloader animation
 function stopLoadingAnimation() {
-    document.getElementById("loading__content").style.animation = "none";
+    document.getElementById("loading__content").style.cssText = "animation: none; display: none;";
 };
