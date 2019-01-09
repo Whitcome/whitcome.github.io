cf6cd8d1f1b8a25eba19ad58a988769f9aedbcdc
Dave <contact@davidwhitcome.com>
2019-01-08 23:20:20 -0600

    Removed jquery dependency

--- scripts/dave.js
+++ scripts/dave.js
@@ -1,35 +1,28 @@
-// Preloader
+// Loading
 var pageLoaded = false;
-
-$(document).ready(function () {
-    if (!pageLoaded) {
+document.addEventListener("DOMContentLoaded", function () {
+    if (!pageLoaded)
         document.getElementById("body__content").style.opacity = "0";
-    }
 });
 
-window.onload = function () {
+window.addEventListener("load", function () {
     pageLoaded = true;
-    ParallaxAnimation($("#header__background"));
+    ParallaxAnimation(); // Set header background to correct position
     document.getElementById("body__content").style.transition = "ease-out 0.2s opacity";
     document.getElementById("body__content").style.opacity = "1";
     setTimeout(stopLoadingAnimation, 400);
-};
+});
 
 function stopLoadingAnimation() {
     document.getElementById("preloader").style.cssText = "animation: none; display: none;";
 };
 
 // Scrolling
-document.addEventListener('DOMContentLoaded', function () {
-    const moving__background = $("#header__background");
-    window.addEventListener('scroll', function () {
-        if (document.body.scrollTop >= 0)
-            ParallaxAnimation(moving__background);
-        else
-            moving__background.css('margin-top', 0);
-    });
-});
+window.addEventListener('scroll', ParallaxAnimation);
 
-function ParallaxAnimation(el) {
-    el.css('margin-top', ($(window).scrollTop() / 4)); // Parallax scrolling
+function ParallaxAnimation() {
+    if (document.body.scrollTop >= 0)
+        document.getElementById("header__background").style.marginTop = (window.pageYOffset / 4) + "px";
+    else
+        header__background.style.marginTop = "0px";
 };
