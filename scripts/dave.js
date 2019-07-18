da0ffe4da413536f8298e44fd646b1cba7309205
Dave <contact@davidwhitcome.com>
2019-07-17 22:06:34 -0500

    prevent header background from scrolling the other way (iOS, etc.)

--- scripts/dave.js
+++ scripts/dave.js
@@ -32,8 +32,9 @@ window.addEventListener('scroll', ParallaxAnimation);
 
 function ParallaxAnimation() {
     if (header__background != null) {
-        if (document.body.scrollTop >= 0)
-            header__background.style.marginTop = (window.pageYOffset / 4) + "px";
+        var backgroundPosition = window.pageYOffset / 4;
+        if (backgroundPosition >= 0)
+            header__background.style.marginTop = backgroundPosition + "px";
         else
             header__background.style.marginTop = "0px";
     }
