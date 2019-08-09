45444267c0db76b0600146e7f5d695e52a80f840
Dave <contact@davidwhitcome.com>
2019-08-08 22:48:09 -0500

    hero-header start

--- davidwhitcomecom/src/app/app.module.ts
+++ davidwhitcomecom/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeroHeaderComponent } from './hero-header/hero-header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroHeaderComponent
   ],
   imports: [
     BrowserModule,
