8129d6d12cc9b45e6c4f60bff084c132a04193d7
Dave <contact@davidwhitcome.com>
2019-08-08 23:11:06 -0500

    setFooterYear()

--- davidwhitcomecom/src/app/footer/footer.component.ts
+++ davidwhitcomecom/src/app/footer/footer.component.ts
@@ -10,6 +10,10 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.setFooterYear();
+  }
+
+  setFooterYear() {
     var footerDate: HTMLElement = document.getElementById("footerDate");
     if (footerDate != null)
         footerDate.innerText = new Date().getFullYear().toString();
