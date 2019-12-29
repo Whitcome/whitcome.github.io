5ef5d6e8f163e6586de4b7f2647533769633978d
Dave <contact@davidwhitcome.com>
2019-12-29 15:58:13 -0600

    remove loading animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,9 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('loading', [
-      state('true', style({
-        opacity: 0
-      })),
-      state('false', style({
-        opacity: 1
-      })),
-      transition('true => false', [
-        animate(200)
-      ])
-    ])
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
 }
