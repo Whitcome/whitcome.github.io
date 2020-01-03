278b636e9481e019aea1138dbabde03740684dff
Dave <contact@davidwhitcome.com>
2020-01-02 20:15:59 -0600

    Angular + Typescript skills

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -16,13 +16,13 @@ export class SkillsComponent {
     this.skills.push(
       new Skill("devicon-csharp-plain", "C#"),
       new Skill("devicon-dot-net-plain", ".NET"),
-      new Skill("devicon-javascript-plain", "JavaScript"),
-      new Skill("devicon-cplusplus-plain", "C++"),
+      new Skill("devicon-angularjs-plain", "Angular"),
+      new Skill("devicon-typescript-plain", "TypeScript"),
       new Skill("devicon-visualstudio-plain", "Visual Studio"),
       new Skill("devicon-git-plain", "Git"),
       new Skill("devicon-html5-plain", "HTML"),
       new Skill("devicon-css3-plain", "CSS"),
-      new Skill("fa fa-database", "SQL"),
+      new Skill("fa fa-database", "SQL")
     );
   }
 }
