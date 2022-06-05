23b3fc5065f14f4d403018968f4d4dbf74651167
Dave <contact@davidwhitcome.com>
2022-06-05 17:09:35 +0000

    Cleanup for parallax animation typescript. Use @ViewChild decorator.

--- src/app/hero-header/hero-header.component.ts
+++ src/app/hero-header/hero-header.component.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero-header',
   templateUrl: './hero-header.component.html',
   styleUrls: ['./hero-header.component.scss']
 })
-export class HeroHeaderComponent implements OnInit {
+export class HeroHeaderComponent implements AfterViewInit {
   Name: string = "David Whitcome";
   Tag: string = "Software Developer";
+  
+  @ViewChild('headerBackground')
+  private headerBackground: ElementRef;
 
-  ngOnInit() {
+  ngAfterViewInit() {
     this.ParallaxAnimation(); // Set header background to correct position for some browsers.
-    window.addEventListener('scroll', this.ParallaxAnimation);
   }
 
+  @HostListener('window:scroll', ['$event'])
   ParallaxAnimation() {
-    var headerBackground: HTMLElement = document.getElementById("header-background-wrapper");
-    if (headerBackground != null) {
-      var backgroundPosition: number = window.pageYOffset / 4;
-      if (backgroundPosition >= 0)
-        headerBackground.style.marginTop = backgroundPosition + "px";
-      else
-        headerBackground.style.marginTop = "0px";
+    if (this.headerBackground) {
+      this.headerBackground.nativeElement.style.marginTop = Math.max(0, window.pageYOffset/4) + "px";
     }
   }
 }
