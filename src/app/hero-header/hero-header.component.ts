90a0142ec74bd458c69b623b58b744b87b55381e
Dave <contact@davidwhitcome.com>
2022-06-07 01:10:26 +0000

    use requestAnimationFrame for parallax animation

--- src/app/hero-header/hero-header.component.ts
+++ src/app/hero-header/hero-header.component.ts
@@ -6,18 +6,22 @@ import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@
   styleUrls: ['./hero-header.component.scss']
 })
 export class HeroHeaderComponent implements AfterViewInit {
-  Name: string = "David Whitcome";
-  Tag: string = "Software Developer";
+  public Name: string = "David Whitcome";
+  public Tag: string = "Software Developer";
   
   @ViewChild('headerBackground')
   private headerBackground: ElementRef;
 
   ngAfterViewInit() {
-    this.ParallaxAnimation(); // Set header background to correct position for some browsers.
+    this.parallaxAnimation(); // Set header background to correct position for some browsers.
   }
 
   @HostListener('window:scroll', ['$event'])
-  ParallaxAnimation() {
+  onWindowScroll() {
+    requestAnimationFrame(this.parallaxAnimation);
+  }
+
+  parallaxAnimation = () => {
     if (this.headerBackground) {
       this.headerBackground.nativeElement.style.marginTop = Math.max(0, window.pageYOffset/4) + "px";
     }
